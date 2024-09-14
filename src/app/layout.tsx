7c85fe133715f03b4dea4e872c0c9ef3d2518e54
ethancantorescapemusic <ethan.cantor99@gmail.com>
2024-09-13 21:17:49 -0400

    add shadcn and darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +15,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`antialiased`}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-stone-700 w-screen h-screen">
+      <Card className="bg-stone-900 w-fit">
+        <CardHeader>
+          header
+          <CardTitle >Title</CardTitle>
+        </CardHeader>
+        <CardContent>
+          content
+          <CardDescription>description</CardDescription>
+        </CardContent>
+        <CardFooter>
+          footer
+        </CardFooter>
+      </Card>
       
     </div>
   );
